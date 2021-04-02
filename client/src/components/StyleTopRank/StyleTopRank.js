0357618990ccbfbc0b17d641d998d4d734ba83bf
geordieanderson <geordiea@hotmail.com>
2021-04-02 16:07:18 -0400

    Updated styling to top ranked beers and added UI functionality

--- client/src/components/StyleTopRank/StyleTopRank.js
+++ client/src/components/StyleTopRank/StyleTopRank.js
@@ -5,7 +5,7 @@ import Untappd from '../../assets/images/Icon-untappd.svg';
 const StyleTopRank = ({beer}) =>{
 
 return (
-    <>      
+    <section>      
         <div className="header__container">
             <h1 className="header__title">TOP RANKED BEERS</h1>
             <img className="header__image" src={Untappd} alt='Untappd'/>
@@ -17,30 +17,34 @@ return (
         return(
         <div key={rank.id}> {/* originally line 22 between card/ranked*/}
             
-        <div className="cardContainer">         
-            <div className="style__card">    
-                <div className="card__detailTop">     
-                    <h4 className="style__name">{rank.beerName}</h4>
-                </div>
-                    
-                <div className="card__detailBottom">
-                    <h4 className="style__brewer">{rank.brewery}</h4>
-                </div>
+        <div className="rank__container">         
+            <div className="rank__card">   
+                <div className="card__top">
+                
+                    <div className="card__left">
+                        <img className='beer__label' src={rank.label} alt={rank.beerName}/>
+                    </div>
+                    <div className="card__right">     
+                        <h4 className="beer__name">{rank.beerName}</h4>
+                        <h4 className="beer__brewer">{rank.brewery}</h4>
+                        <h4 className="beer__rating">{rank.rating}</h4>
+                    </div>
 
-                <div className="card__image">
-                    <img className='style__label' src={rank.label} alt={rank.beerName}/>
-                </div>
-
-                <div className="card__bottom">
-                    <h4 className="style__rating">{rank.rating}</h4>
                 </div> 
+                <div className="card__bottom">
+                    <textarea className="card__notes" type="text" placeholder="Add tasting notes"></textarea>
+                    <div className="button__container">
+                        <button className="button__submit" type="submit">SUBMIT</button>
+                        <button className="button__like" type="submit">LIKE</button>
+                    </div>
+                </div>
             </div>
         </div>
         
         </div>
         )
         })}
-    </> 
+    </section> 
     );   
 };
 
